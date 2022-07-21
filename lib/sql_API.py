f726c1505b00aaba40d0bda32a3940819f487d21
106368030chentairong <t106368030@ntut.edu.tw>
2022-07-21 15:57:42 +0800

    UPDATE

---
+++ lib/sql_API.py
@@ -0,0 +1,8 @@
+import psycopg2
+
+conn = psycopg2.connect(database="d4dv34dbvjjebc",
+						user="xfbufavhetsiza",
+						password="b6d5807952f7033be5f26b4fb48f0166fe1b2a888f443634c3cf322971534814",
+						host="ec2-54-87-179-4.compute-1.amazonaws.com",
+						port="5432")
+print("Opened database successfully")
